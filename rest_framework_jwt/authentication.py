6ec79ac83f839a00afeb3cc106c18898a6ebc276
Nairitya Khilari <nkmann2@gmail.com>
2016-06-13 21:39:29 +0530

    Fixes BaseJWTAuthentication
    define function get_jwt_value inside the BaseJSONWebTokenAuthentication
    class

--- rest_framework_jwt/authentication.py
+++ rest_framework_jwt/authentication.py
@@ -44,6 +44,26 @@ class BaseJSONWebTokenAuthentication(BaseAuthentication):
 
         return (user, jwt_value)
 
+    def get_jwt_value(self, request):
+        """
+        This function returns the JWT encoded string from the Authorization header.
+        """
+        auth = get_authorization_header(request).split()
+        auth_header_prefix = api_settings.JWT_AUTH_HEADER_PREFIX.lower()
+
+        if not auth or smart_text(auth[0].lower()) != auth_header_prefix:
+            return None
+
+        if len(auth) == 1:
+            msg = _('Invalid Authorization header. No credentials provided.')
+            raise exceptions.AuthenticationFailed(msg)
+        elif len(auth) > 2:
+            msg = _('Invalid Authorization header. Credentials string '
+                    'should not contain spaces.')
+            raise exceptions.AuthenticationFailed(msg)
+
+        return auth[1]
+
     def authenticate_credentials(self, payload):
         """
         Returns an active user that matches the payload's user id and email.
